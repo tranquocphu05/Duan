867a5559d00f7efea032c1c3c757b06c0bbd3f1a
quocphudev05 <tranquocphu17102005@gmail.com>
2025-02-11 11:17:50 +0700

    Cập nhật code mới

--- configs/env.php
+++ configs/env.php
@@ -1,33 +1,10 @@
 <?php
+//Khai báo môi trường, các biến dùng chung
+//Khai báo dạng hằng số
 
-define('BASE_URL',          'http://localhost/MVC/');
-define('BASE_URL_ADMIN',    'http://localhost/MVC/?mode=admin');
-
-define('PATH_ROOT',         __DIR__ . '/../');
-
-define('PATH_VIEW_ADMIN',   PATH_ROOT . 'views/admin/');
-define('PATH_VIEW_CLIENT',  PATH_ROOT . 'views/client/');
-
-define('PATH_VIEW_ADMIN_MAIN',    PATH_ROOT . 'views/admin/main.php');
-define('PATH_VIEW_CLIENT_MAIN',   PATH_ROOT . 'views/client/main.php');
-
-define('BASE_ASSETS_ADMIN',     BASE_URL . 'assets/admin/');
-define('BASE_ASSETS_CLIENT',    BASE_URL . 'assets/client/');
-define('BASE_ASSETS_UPLOADS',   BASE_URL . 'assets/uploads/');
-
-define('PATH_ASSETS_UPLOADS',   PATH_ROOT . 'assets/uploads/');
-
-define('PATH_CONTROLLER_ADMIN',     PATH_ROOT . 'controllers/admin/');
-define('PATH_CONTROLLER_CLIENT',    PATH_ROOT . 'controllers/client/');
-
-define('PATH_MODEL', PATH_ROOT . 'models/');
-
-define('DB_HOST',     'localhost');
-define('DB_PORT',     '3306');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME',     'nhom7_duan1');
-define('DB_OPTIONS', [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-]);
+define('DB_HOST','localhost');
+define('DB_PORT','3306');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','duan1_nhom7');
+?>
--- index.php
+++ index.php
@@ -1,35 +1,76 @@
-<?php 
-
+<?php
 session_start();
 
-spl_autoload_register(function ($class) {    
-    $fileName = "$class.php";
+// Nhúng các file cần dùng vào
 
-    $fileModel              = PATH_MODEL . $fileName;
-    $fileControllerClient   = PATH_CONTROLLER_CLIENT . $fileName;
-    $fileControllerAdmin    = PATH_CONTROLLER_ADMIN . $fileName;
+//Common
 
-    if (is_readable($fileModel)) {
-        require_once $fileModel;
-    } 
-    else if (is_readable($fileControllerClient)) {
-        require_once $fileControllerClient;
-    }
-    else if (is_readable($fileControllerAdmin)) {
-        require_once $fileControllerAdmin;
-    }
-});
+include "common/env.php";
+include "common/function.php";
 
-require_once './configs/env.php';
-require_once './configs/helper.php';
+// Nhứng troàn bộ các file controller
+include "controller/HomeController.php";
+include "controller/LoginController.php";
+include "admin/controller/categoryController.php";
 
-// Điều hướng
-$mode = $_GET['mode'] ?? 'client';
+// Nhứng troàn bộ các file model
 
-if ($mode == 'admin') {
-    # require đường dẫn của admin
-    require_once './routes/admin.php';
-} else {
-    # require đường dẫn của client
-    require_once './routes/client.php';
+
+include 'admin/model/product.php';
+include "model/productQuery.php";
+include "model/loginQuery.php";
+include "model/billQuery.php";
+include "model/order.php";
+// include "model/account.php";
+
+
+include "admin/model/category.php"; 
+include "admin/model/categoryQuery.php"; 
+include "admin/model/account.php"; 
+include "admin/model/accountQuery.php"; 
+include "admin/model/bill.php"; 
+include "admin/model/comment.php";
+include "admin/model/commentQuery.php";
+include "admin/model/news.php";
+include "admin/model/newsQuery.php";
+include "admin/model/voucher.php";
+include "admin/model/voucherQuery.php";
+
+
+// Thông tin act
+$act = $_GET['act'] ?? '' ;
+$id = $_GET['id']  ?? '';
+date_default_timezone_set('Asia/Jakarta');
+
+if(!isset($_SESSION["myCart"])) {
+  $_SESSION["myCart"] = [];
 }
+
+match ($act) {
+    '' => (new HomeController())->home(),
+    'login' => (new LoginController) ->login(),
+    'logout' => (new LoginController()) ->logout(),
+    'signup' => (new LoginController()) ->signup(),
+    // 'dangky' => (new LoginController()) ->dangky(),
+    'cart' =>  (new HomeController())->cart(),
+    'ctsp'=> (new HomeController()) -> ctsp(),
+    'order' => (new HomeController()) -> order(),
+    'end_order' => (new HomeController()) -> end_order(),
+    // 'ctsp_dt' => (new HomeController()) -> ctsp_dt(),
+
+    'deleteProInCart' => (new HomeController()) -> deleteOneProInCart(),
+
+    'deleteAllCart' => (new HomeController()) -> deleteAllCart(),
+
+    'view_profile' => (new HomeController()) -> viewProfile(),
+
+    'update_profile' => (new HomeController()) -> updateProfile(),
+    
+    'searchPro' => (new HomeController()) -> searchPro(),
+
+    'showAllProOfCate'=> (new HomeController()) -> showAllProOfCate(),
+    
+}
+
+
+?>
